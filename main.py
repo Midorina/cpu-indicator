dadbb11dff11700af3ece7c2e37836b966feee0e
Midoriin <fbyigit07@hotmail.com>
2021-04-24 23:17:21 +0300

    rewrite

--- main.py
+++ main.py
@@ -1,82 +1,74 @@
-from psutil import cpu_percent
+import math
+import time
+from threading import Thread
+
 import pystray
 from PIL import Image, ImageDraw, ImageFont
-from threading import Thread
-import os
-
-icon = None
-
-font = ImageFont.truetype('arial.ttf', 75)
+from psutil import cpu_percent
 
 
-def exit_action(icon):
-    icon.visible = False
-    icon.stop()
-    os._exit(1)
+class Indicator:
+    # changeable variables
+    REFRESH_INTERVAL = 0.5
+    FONT_SIZE = 200
+    ICON_RES = 250, 250
+
+    FONT = ImageFont.truetype('arial.ttf', FONT_SIZE)
+    BLUE_BG = Image.new('RGB', ICON_RES, color=(61, 75, 129))
+    RED_BG = Image.new('RGB', ICON_RES, color=(255, 0, 0))
+
+    def __init__(self):
+        self.icon = pystray.Icon("usage", title="CPU Usage")
+        self.icon.menu = pystray.Menu(pystray.MenuItem('Exit', lambda: self.exit_action()))
+
+        self.refresh_icon_thread = None
+
+        # this is so that our icon refresher thread knows when to stop
+        self.stopped = False
+
+    def exit_action(self):
+        self.icon.stop()
+        self.stopped = True
+
+    def refresh_icon(self):
+        while True:
+            if self.stopped is True:
+                return
+
+            usage = math.ceil(cpu_percent(interval=0))
+
+            # decide bg depending on usage
+            image = self.RED_BG.copy() if usage > 70 else self.BLUE_BG.copy()
+
+            # draw the usage text over bg
+            draw = ImageDraw.Draw(image)
+            draw.text(
+                # center the usage text
+                xy=(
+                    self.ICON_RES[0] / 2,
+                    self.ICON_RES[1] / 2
+                ),
+                anchor="mm",  # https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html#text-anchors
+
+                text=str(usage),
+                font=self.FONT,
+                fill=(255, 255, 255),
+            )
+
+            self.icon.icon = image
+
+            time.sleep(self.REFRESH_INTERVAL)
+
+    def start(self):
+        self.refresh_icon_thread = Thread(daemon=True, target=self.refresh_icon)
+        self.refresh_icon_thread.start()
+
+        # wait a bit so that an icon is set
+        time.sleep(0.5)
+
+        self.icon.run()
+
+        self.refresh_icon_thread.join()
 
 
-def refresh_images():
-    global blue_img
-    blue_img = Image.new('RGB', (100, 100), color=(61, 75, 129))
-    global red_img
-    red_img = Image.new('RGB', (100, 100), color=(255, 0, 0))
-
-
-def decide_bg(usage):
-    if usage > 70:
-        return red_img
-    else:
-        return blue_img
-
-
-def decide_coords(usage):
-    if usage < 10:
-        return (32, 5)
-    elif usage == 100:
-        return (0, 5)
-    else:
-        return (5, 5)
-
-
-def start_icon():
-    usage = int(cpu_percent(interval=1))
-    refresh_images()
-    image = decide_bg(usage)
-    coords = decide_coords(usage)
-
-    d = ImageDraw.Draw(image)
-    d.text(coords, str(usage), font=font, fill=(255, 255, 255))
-
-    global icon
-    icon = pystray.Icon("usage", icon=image, title="CPU Usage")
-    icon.menu = pystray.Menu(pystray.MenuItem('Exit', lambda: exit_action(icon)))
-
-    icon.run()
-
-
-def refresh_icon():
-    while True:
-        usage = int(cpu_percent(interval=1))
-        refresh_images()
-        image = decide_bg(usage)
-        coords = decide_coords(usage)
-
-        d = ImageDraw.Draw(image)
-        d.text(coords, str(usage), font=font, fill=(255, 255, 255))
-
-        try:
-            icon.icon = image
-        except:
-            pass
-
-
-thread1 = Thread(daemon=True, target=start_icon)
-thread2 = Thread(daemon=True, target=refresh_icon)
-
-thread1.start()
-thread2.start()
-
-thread2.join()
-thread1.join()
-
-exit()
+Indicator().start()
